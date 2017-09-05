d3367bccd5e8c23d77d057fa17ca7683ff695851
Benjamin Aschenbrenner <baschenbrenner@hotmail.com>
2017-09-05 16:00:59 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,20 @@
+function getFirstSelector(selector) {
+  return document.querySelector(selector)
+}
+
+function nestedTarget()
+{
+return document.getElementById('nested').querySelector('div.target')
+
+}
+
+function deepestChild() {
+  return document.getElementById('grand-node').querySelector('div div div div div')
+}
+
+function increaseRankBy(n) {
+  const lis = document.getElementById('app').querySelectorAll('ul.ranked-list li')
+
+  for (let i = 0; i < lis.length; i++) {
+    lis[i].innerHTML = (parseInt(lis[i].innerHTML) + n).toString()
+  } }
